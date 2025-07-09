cd34516513223210c0b58b706b87914676bc8c43
kyzotom <Kyzo@post.sk>
2025-07-09 15:38:37 +0200

    Update Index.js

--- api/Index.js
+++ api/Index.js
@@ -1,40 +1,28 @@
 const { addonBuilder } = require("stremio-addon-sdk");
-
-const manifest = {
-  id: "org.streamuj.tv",
-  version: "1.2.0",
-  name: "StreamujTV (Sosac) Demo",
-  description: "Demo addon pre prehrávanie streamov vo Stremio",
-  catalogs: [
-    {
-      type: "movie",
-      id: "streamuj-catalog",
-      name: "Streamuj.tv",
-      extra: [{ name: "search" }]
-    }
-  ],
-  resources: ["catalog", "stream"],
-  types: ["movie"],
-  idPrefixes: ["tt"]
-};
-
-const builder = new addonBuilder(manifest);
-
-builder.defineCatalogHandler((args) => {
-  // Prázdny katalóg (iba pre demo)
-  return Promise.resolve({ metas: [] });
+const builder = new addonBuilder({
+    id: "org.streamuj.tv",
+    version: "1.2.0",
+    name: "StreamujTV (Sosac) Demo",
+    description: "Demo addon pre prehrávanie streamov vo Stremio",
+    resources: ["catalog", "stream"],
+    types: ["movie"],
+    idPrefixes: ["tt"],
+    catalogs: [
+        {
+            type: "movie",
+            id: "streamuj-catalog",
+            name: "Streamuj.tv",
+            extra: [{ name: "search" }]
+        }
+    ]
 });
 
-builder.defineStreamHandler(async ({ id }) => {
-  // Testovací stream
-  return {
+builder.defineCatalogHandler(() => Promise.resolve({ metas: [] }));
+builder.defineStreamHandler(() => Promise.resolve({
     streams: [{
-      title: "Test Stream (DEMO)",
-      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+        title: "Test Stream (DEMO)",
+        url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
     }]
-  };
-});
-
-const iface = builder.getInterface();
+}));
 
 module.exports = (req, res) => iface(req, res);
