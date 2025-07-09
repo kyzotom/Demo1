a7fd3f61740bd8961e9070d5ddc0e3c2b03d4e66
kyzotom <Kyzo@post.sk>
2025-07-09 12:07:01 +0200

    Create Manifest.js

---
+++ Api/Manifest.js
@@ -0,0 +1,41 @@
+const { addonBuilder } = require("stremio-addon-sdk");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+const manifest = {
+  id: "org.streamuj.tv",
+  version: "1.2.0",
+  name: "StreamujTV (Sosac) Premium",
+  description: "Addon pre prehrávanie streamuj.tv (Sosáč) podľa titulov z TMDB/Cinemeta",
+  catalogs: [
+    {
+      type: "movie",
+      id: "streamuj-catalog",
+      name: "Streamuj.tv",
+      extra: [{ name: "search" }]
+    }
+  ],
+  resources: ["catalog", "stream"],
+  types: ["movie"],
+  idPrefixes: ["tt"]
+};
+
+const builder = new addonBuilder(manifest);
+
+builder.defineCatalogHandler((args) => {
+  return Promise.resolve({ metas: [] });
+});
+
+builder.defineStreamHandler(async ({ id }) => {
+  return {
+    streams: [{
+      title: "Test Stream",
+      url: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+    }]
+  };
+});
+
+// Toto je Vercel API handler!
+module.exports = (req, res) => {
+  return builder.getInterface()(req, res);
+};
