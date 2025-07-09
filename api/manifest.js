52a7b2766fa3ec89d284c9b31ce932d2bdcebaaa
kyzotom <Kyzo@post.sk>
2025-07-09 17:43:46 +0200

    Update manifest.js

--- api/manifest.js
+++ api/manifest.js
@@ -1,20 +1,22 @@
+const manifest = {
+  id: "org.streamuj.tv",
+  version: "1.2.0",
+  name: "StreamujTV (Sosac) Demo",
+  description: "Demo addon pre prehrávanie streamov vo Stremio",
+  catalogs: [
+    {
+      type: "movie",
+      id: "streamuj-catalog",
+      name: "Streamuj.tv",
+      extra: [{ name: "search" }]
+    }
+  ],
+  resources: ["catalog", "stream"],
+  types: ["movie"],
+  idPrefixes: ["tt"]
+};
+
 module.exports = (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({
-        id: "org.streamuj.tv",
-        version: "1.2.0",
-        name: "StreamujTV (Sosac) Demo",
-        description: "Demo addon pre prehrávanie streamov vo Stremio",
-        resources: ["catalog", "stream"],
-        types: ["movie"],
-        idPrefixes: ["tt"],
-        catalogs: [
-            {
-                type: "movie",
-                id: "streamuj-catalog",
-                name: "Streamuj.tv",
-                extra: [{ name: "search" }]
-            }
-        ]
-    }));
+  res.setHeader("Content-Type", "application/json");
+  res.end(JSON.stringify(manifest));
 };
